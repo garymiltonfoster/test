de277aca45602709d3cc8d42bd62b8d9bcafd699
garymiltonfoster <142818681+garymiltonfoster@users.noreply.github.com>
2024-01-22 13:41:33 +0700

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test {
     public static void main(String[] args) {
         // Print "hello" to the console
-        System.out.println("hello");
+        System.out.println("hello 1");
     }
 }
